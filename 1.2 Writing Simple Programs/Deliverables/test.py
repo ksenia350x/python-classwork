993b044daf3beca0ed67453e86817bacd4ad0f8e
ksenia350x <ksenia350x@gmail.com>
2019-01-02 07:44:37 -0500

    Added to 1.3-challenges-05

---
+++ 1.2 Writing Simple Programs/Deliverables/test.py
@@ -0,0 +1,6 @@
+def main():
+	print("This program converts US Dollars to Euros")
+	gnput1 = eval(input("Enter the amount of US Dollars to convert: "))
+	output = gnput1 * .89
+	print("$"+gnput1,"=",output,"Euros")
+main()
